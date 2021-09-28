13d21b264e98873cdd148c73bc2fd9faf9c8cc9c
Pedro Pires <plpires99@gmail.com>
2021-09-28 22:31:43 +0100

    Votes are counted 1 per user

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,31 @@ use twitch_irc::ClientConfig;
 use twitch_irc::SecureTCPTransport;
 use twitch_irc::TwitchIRCClient;
 use twitch_irc::login::StaticLoginCredentials;
+use twitch_irc::message::PrivmsgMessage;
 use twitch_irc::message::ServerMessage;
+use twitch_irc::message::TwitchUserBasics;
 
 struct Votes (i32, i32);
 
-fn is_mod(username: &String) -> bool {
-    username == "onelikeandidie"
+fn is_mod(sender: &TwitchUserBasics) -> bool {
+    sender.id == "207883858"
 }
 
 struct State {
     is_counting: bool,
-    voting_box: Votes
+    voting_box: Votes,
+    who_voted: Vec<String>,
+    bot_name: String,
+    channel_name: String
 }
 
 impl State {
-    fn to_string(&self) -> String{
+    fn to_string(&self) -> String {
         let strings = [self.voting_box.0.to_string(), " voted yes, ".to_owned(), self.voting_box.1.to_string(), " voted no!".to_owned()];
         strings.concat()
     }
 
-    fn add_vote(&mut self, amount: i32, to: i8) {
+    fn add_vote(&mut self, amount: i32, to: i8, voter: &TwitchUserBasics) {
         match to {
             0 => {
                 self.voting_box.0 = self.voting_box.0 + amount;
@@ -31,23 +36,75 @@ impl State {
             }
             _ => {}
         }
+        self.who_voted.push(voter.id.clone())
+    }
+
+    fn can_vote(&self, voter: &TwitchUserBasics) -> bool {
+        self.is_counting && !self.who_voted.contains(&voter.id)
+    }
+}
+
+async fn handle_message(client: &TwitchIRCClient<SecureTCPTransport, StaticLoginCredentials>, state: &mut State, msg: &PrivmsgMessage) {
+    match msg.message_text.as_str() {
+        "1" => {
+            if state.can_vote(&msg.sender) {
+                state.add_vote(1, 0, &msg.sender);
+            }
+        }
+        "2" => {
+            if state.can_vote(&msg.sender) {
+                state.add_vote(1, 1, &msg.sender);
+            }
+        }
+        "!results" => {
+            if is_mod(&msg.sender) {
+                state.is_counting = false;
+                let message = [
+                    "@".to_owned(), 
+                    msg.sender.name.clone().to_owned(), 
+                    " ".to_owned(), 
+                    state.to_string()
+                    ].concat();
+                    client.say(state.channel_name.to_owned(), message).await.unwrap();
+            }
+        }
+        "!reset" => {
+            if is_mod(&msg.sender) {
+                state.is_counting = true;
+                state.voting_box = Votes(0,0);
+                state.who_voted = Vec::new();
+                client.say(state.channel_name.to_owned(), "Reset votes!".to_owned()).await.unwrap();
+            }
+        }
+        "!stop" => {
+            if is_mod(&msg.sender) {
+                state.is_counting = false;
+                client.say(state.channel_name.to_owned(), "Stopped counting!".to_owned()).await.unwrap();
+            }
+        }
+        _ => {}
     }
 }
 
 #[tokio::main]
 pub async fn main() {
-    let login_name = "onelikeandishutdown".to_owned();
     let oauth_token = "5dqam1yud0u6f962xblrkzfy8pid1n".to_owned();
-    let channel_login = "onelikeandidie";
+    let bot_name = "onelikeandishutdown".to_owned();
+    let channel_name = "onelikeandidie".to_owned();
+
+    let mut state: State = State{
+        is_counting: true, 
+        voting_box: Votes(0,0), 
+        who_voted: Vec::new(),
+        bot_name: bot_name.clone(), 
+        channel_name: channel_name.clone()};
     
     let config = ClientConfig::new_simple(
-        StaticLoginCredentials::new(login_name, Some(oauth_token))
+        StaticLoginCredentials::new(state.bot_name.clone(), Some(oauth_token))
     );
 
     let (mut incoming_messages, client) =
         TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
-
-    let mut state: State = State{is_counting: true, voting_box: Votes(0,0)};
 
     // first thing you should do: start consuming incoming messages,
     // otherwise they will back up.
@@ -57,44 +114,7 @@ pub async fn main() {
             //println!("Received message: {:?}", message);
             match message {
                 ServerMessage::Privmsg(msg) => {
-                    match msg.message_text.as_str() {
-                        "1" => {
-                            if state.is_counting {
-                                state.add_vote(1, 0);
-                            }
-                        }
-                        "2" => {
-                            if state.is_counting {
-                                state.add_vote(1, 1);
-                            }
-                        }
-                        "!results" => {
-                            if is_mod(&msg.sender.name) {
-                                state.is_counting = false;
-                                let message = [
-                                    "@".to_owned(), 
-                                    msg.sender.name.clone().to_owned(), 
-                                    " ".to_owned(), 
-                                    state.to_string()
-                                    ].concat();
-                                thread_client.say(channel_login.to_owned(), message).await.unwrap();
-                            }
-                        }
-                        "!reset" => {
-                            if is_mod(&msg.sender.name) {
-                                state.is_counting = true;
-                                state.voting_box = Votes(0,0);
-                                thread_client.say(channel_login.to_owned(), "Reset votes!".to_owned()).await.unwrap();
-                            }
-                        }
-                        "!stop" => {
-                            if is_mod(&msg.sender.name) {
-                                state.is_counting = false;
-                                thread_client.say(channel_login.to_owned(), "Stopped counting!".to_owned()).await.unwrap();
-                            }
-                        }
-                        _ => {}
-                    }
+                    handle_message(&thread_client, &mut state, &msg).await;
                 },
                 //ServerMessage::ClearChat(_) => todo!(),
                 //ServerMessage::ClearMsg(_) => todo!(),
@@ -117,7 +137,7 @@ pub async fn main() {
     });
 
     // join a channel
-    client.join(channel_login.to_owned());
+    client.join(channel_name.clone().to_owned());
 
     // keep the tokio executor alive.
     // If you return instead of waiting the background task will exit.
